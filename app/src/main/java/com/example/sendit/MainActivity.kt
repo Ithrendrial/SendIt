a3ed22eea4b7531f5ff8828b8999416052b9ae74
Ithrendrial <rcox2990@gmail.com>
2026-09-18 13:48:40 +1200

    feat: add interactive attempt form landing page

--- app/src/main/java/com/example/sendit/MainActivity.kt
+++ app/src/main/java/com/example/sendit/MainActivity.kt
@@ -8,10 +8,8 @@ import androidx.activity.enableEdgeToEdge
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.padding
 import androidx.compose.material3.Scaffold
-import androidx.compose.material3.Text
-import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.tooling.preview.Preview
+import com.example.sendit.ui.screens.AttemptFormScreen
 import com.example.sendit.ui.theme.SendItTheme
 
 class MainActivity : ComponentActivity() {
@@ -24,28 +22,11 @@ class MainActivity : ComponentActivity() {
         setContent {
             SendItTheme {
                 Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
-                    Greeting(
-                        name = "Android",
+                    AttemptFormScreen(
                         modifier = Modifier.padding(innerPadding)
                     )
                 }
             }
         }
-    }
-}
-
-@Composable
-fun Greeting(name: String, modifier: Modifier = Modifier) {
-    Text(
-        text = "Hello $name!",
-        modifier = modifier
-    )
-}
-
-@Preview(showBackground = true)
-@Composable
-fun GreetingPreview() {
-    SendItTheme {
-        Greeting("Android")
     }
 }
